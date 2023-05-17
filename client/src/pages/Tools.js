7830fda2a1c32587141b8b457294ec8f8e401b8c
dkorablev <dkorablev@toyota-ufa.ru>
2023-05-17 14:02:14 +0500

    Название страницы Admin заменено на Tools

--- client/src/pages/Tools.js
+++ client/src/pages/Tools.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Admin = () => {
+const Tools = () => {
     return (
         <div>
            Admin Page
@@ -8,4 +8,4 @@ const Admin = () => {
     );
 };
 
-export default Admin;
+export default Tools;
