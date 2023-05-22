2e128b9a5a67a42c4b8e6896552fc7586a608a3f
dkorablev <dkorablev@toyota-ufa.ru>
2023-05-22 17:05:09 +0500

    Добавлен UserStore

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Routes, Route,Navigate} from "react-router-dom";
 import {adminRoutes, rootRoutes, userRoutes} from "../routes";
 import {LOGIN_ROUTE} from "../utils/consts";
+import {Context} from "../index";
 
 
 const AppRouter = () => {
-    const isAuth = false
-    console.log(userRoutes)
+    const {user} = useContext(Context)
+    console.log(user)
     return (
         <Routes>
             {userRoutes.map(({path, component}) =>
                 <Route key={path} path={path} Component={component} exact />
             )}
-            {isAuth && adminRoutes.map(({path, component}) =>
+            {adminRoutes.map(({path, component}) =>
                 <Route key={path} path={path} Component={component} exact />
             )}
-            {isAuth && rootRoutes.map(({path, component}) =>
+            {rootRoutes.map(({path, component}) =>
                 <Route key={path} path={path} Component={component} exact />
             )}
-            <Route
-                path="*"  element={<Navigate to={LOGIN_ROUTE} replace />}
-            />
+            <Route path="*"  element={<Navigate to={LOGIN_ROUTE} replace />}/>
         </Routes>
     );
 };
