a833e593f3fc333248674aadb2b7e13ccf7f08a5
dkorablev <dkorablev@toyota-ufa.ru>
2023-05-22 17:58:56 +0500

    Добавлена форма авторизации

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import {Button, Container, Form} from "react-bootstrap"
+import Card from "react-bootstrap/Card";
+
+
 
 const Login = () => {
     return (
-        <div>
-           Login Page
-        </div>
+        <Container
+            className=" d-flex justify-content-center align-items-center"
+            style={{height: window.innerHeight}}
+        >
+           <Card style={{width: 600}} className="p-4">
+               <h2 className="m-auto">Авторизация</h2>
+               <Form className="d-flex flex-column">
+                   <Form.Control className="mt-3" placeholder="Введите логин"  />
+                   <Form.Control type="password" className="mt-3" placeholder="Введите пароль" />
+                   <Button className="mt-3 align-self-end"> Войти </Button>
+
+               </Form>
+           </Card>
+        </Container>
     );
 };
 
