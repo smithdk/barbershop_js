28d4e22c50442470b8a09a670237e9d35a8dff28
dkorablev <dkorablev@toyota-ufa.ru>
2023-05-25 16:40:52 +0500

    Добавлен компонент Alert для вывода сообщений

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Button, Container, Form} from "react-bootstrap"
 import Card from "react-bootstrap/Card";
-
-
+//import {useLocation,useNavigate} from "react-router-dom";
+import AlertMessage from "../components/AlertMessage"
 
 const Login = () => {
+//    const location = useLocation()
+//    const navigate = useNavigate()
+    const [alertVisible, setAlertVisible] = useState(false)
+
+    const handleClick = () => {
+        setAlertVisible(!alertVisible)
+    }
+
     return (
-        <Container
-            className=" d-flex justify-content-center align-items-center"
-            style={{height: window.innerHeight}}
+    <>
+      <Container fluid className="fixed-top p-0" >
+         <AlertMessage show={alertVisible} message="Ошибка 1" type="danger"/>
+       </Container>
+       <Container
+            className="d-flex justify-content-center align-items-center "
+            style={{height: window.innerHeight - 100}}
         >
            <Card style={{width: 600}} className="p-4">
                <h2 className="m-auto">Авторизация</h2>
                <Form className="d-flex flex-column">
                    <Form.Control className="mt-3" placeholder="Введите логин"  />
                    <Form.Control type="password" className="mt-3" placeholder="Введите пароль" />
-                   <Button className="mt-3 align-self-end"> Войти </Button>
-
+                   <Button className="mt-3 align-self-end" onClick={handleClick} > Войти </Button>
                </Form>
            </Card>
-        </Container>
+      </Container>
+    </>
     );
 };
 
