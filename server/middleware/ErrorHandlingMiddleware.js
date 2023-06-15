490ff8489a7b33847bfa9e7ae98600508a97a630
dkorablev <dkorablev@toyota-ufa.ru>
2023-06-15 17:22:34 +0500

    Добавлена обработка ошибок1

--- server/middleware/ErrorHandlingMiddleware.js
+++ server/middleware/ErrorHandlingMiddleware.js
@@ -1,8 +1,9 @@
 const  ApiError = require('../error/ApiError')
 
-module.exports = function (err, req, res/*, next*/) {
+module.exports = function (err, req, res, next) {
     if (err instanceof ApiError) {
         return res.status(err.status).json({message: err.message})
     }
+    console.log(next) //Выводим, чтобы не было ошибки при commit е
     return res.status(500).json({message: "Непредвиденная ошибка"})
 }
